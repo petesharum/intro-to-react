810f6fd7a804bf858fd69f6e4a0715d16a099ea1
Ryan Boone <ryan.boone@yum.com>
2024-03-25 14:33:59 -0500

    Lazy load routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,78 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Layout } from './screens/layout';
 import { ErrorPage } from './screens/error-page';
-import { Home } from './screens/home';
-import { Menu } from './screens/menu';
-import { ProductDetail } from './screens/product-detail';
-import { Cart } from './screens/cart';
-import { Checkout } from './screens/checkout';
 import { CartProvider } from './lib/cart/cart-provider';
+
+const Home = lazy(() =>
+  import('./screens/home').then((module) => ({ default: module.Home })),
+);
+const Menu = lazy(() =>
+  import('./screens/menu').then((module) => ({ default: module.Menu })),
+);
+const ProductDetail = lazy(() =>
+  import('./screens/product-detail').then((module) => ({
+    default: module.ProductDetail,
+  })),
+);
+const Cart = lazy(() =>
+  import('./screens/cart').then((module) => ({ default: module.Cart })),
+);
+const Checkout = lazy(() =>
+  import('./screens/checkout').then((module) => ({ default: module.Checkout })),
+);
+
+function Loading() {
+  return <div>Loading...</div>;
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/menu', element: <Menu /> },
-      { path: '/menu/:id', element: <ProductDetail /> },
-      { path: '/cart', element: <Cart /> },
-      { path: '/checkout', element: <Checkout /> },
+      {
+        path: '/',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/menu',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Menu />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/menu/:id',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <ProductDetail />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/cart',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Cart />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/checkout',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Checkout />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
