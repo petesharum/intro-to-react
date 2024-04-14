ed9f487aeec1ba41cce2a72aad83661d667d2255
Ryan Boone <ryan.boone@yum.com>
2024-04-14 10:53:37 -0500

    Move global layout to shared components

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Loader2 } from 'lucide-react';
 
-import { GlobalLayout } from './global-layout';
+import { GlobalLayout } from './lib/shared-components/global-layout';
 import { ErrorPage } from './screens/error-page';
 import { CartProvider } from './lib/cart-context';
 
